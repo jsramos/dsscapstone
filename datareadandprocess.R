77b100007e2527d2dfc347723aa0508bb6ae1514
jsramos <ramos.cardona@gmail.com>
2015-10-13 01:56:28 -0500

    2nd upload.

--- datareadandprocess.R
+++ datareadandprocess.R
@@ -6,8 +6,9 @@ library(data.table)
 library(plyr)
 library(dplyr)
 
-# Read-in data
-# Register cores
+# Read-in data. Make sure you've downloaded it from
+# https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/yelp_dataset_challenge_academic_dataset.zip
+# Register cores for parallel processing
 registerDoParallel(cores=2)
 # File prefixes
 filePrefix <- './yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_'
@@ -17,8 +18,16 @@ sets <- c('business','checkin','review','tip','user')
 filenames <- paste(filePrefix, sets, '.json', sep = '')
 # Read them all in one go
 rawstrings <- llply(as.list(filenames), function(x) {read_lines(x)})
+# Save this file as an RDS
+saveRDS(rawstrings, file='rawstrings.RDS')
+# Load this RDS to remove dependency on physical files being present
+newstrings <- readRDS('./rawstrings.RDS')
+# See if they are identical
+if (!identical(rawstrings, newstrings)) {
+     stop('Error loading RDS.')   
+}
 
 # Transform business dataset
 # See http://mkseo.pe.kr/stats/?p=898
-bizdata <- sapply(rawstrings[[1]], function(x) {fromJSON(x)})
+# bizdata <- sapply(rawstrings[[1]], function(x) {fromJSON(x)})
 
